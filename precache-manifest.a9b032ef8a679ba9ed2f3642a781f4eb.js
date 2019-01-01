30118a4df720acb3a80cb2e4000980d01b93af8c
HuangShao1314 <1052912975@qq.com>
2019-01-01 23:14:27 +0800

    update logo.svg

---
+++ precache-manifest.a9b032ef8a679ba9ed2f3642a781f4eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-antd-redux/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "971a7445c1d7614360dc",
+    "url": "/react-antd-redux/static/js/runtime~main.971a7445.js"
+  },
+  {
+    "revision": "8499dc0274f155cdfb4e",
+    "url": "/react-antd-redux/static/js/main.8499dc02.chunk.js"
+  },
+  {
+    "revision": "8c7e6e22eb398d77e78c",
+    "url": "/react-antd-redux/static/js/1.8c7e6e22.chunk.js"
+  },
+  {
+    "revision": "8499dc0274f155cdfb4e",
+    "url": "/react-antd-redux/static/css/main.0553e6f7.chunk.css"
+  },
+  {
+    "revision": "8c7e6e22eb398d77e78c",
+    "url": "/react-antd-redux/static/css/1.8f8af754.chunk.css"
+  },
+  {
+    "revision": "64eff05ae3c674f8cfcc802beebadd88",
+    "url": "/react-antd-redux/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-antd-redux/precache-manifest.4db13e4b8321671a99a641bbd39f6e82.js"
+  "/react-antd-redux/precache-manifest.a9b032ef8a679ba9ed2f3642a781f4eb.js"
 );
 
 workbox.clientsClaim();
